c57ef669e465cf79e0804790aa6f7c3a3c8624e1
NIDA FATIMA <nidafatima2645@gmail.com>
2025-12-03 21:17:38 +0530

    cwe79 Updated

--- cwe79_vuln.cpp
+++ cwe79_vuln.cpp
@@ -9,22 +9,8 @@ int main() {
     cout << "Enter text to write into the HTML file: ";
     getline(cin, userInput);
 
-    ofstream fout("comments.txt");
-    fout << userInput;
-    fout.close();
-
-    ofstream htmlout("vulnerable79.html");
-    ifstream fin("comments.txt");
-    string comment;
-
-    htmlout << "<html><body>\n";
-    while (getline(fin, comment)){
-        htmlout << "<p>" << comment << "</p>\n";
-
-    }
-    htmlout << "</body></html>";
-    fin.close();
-    htmlout.close();
+    ofstream f("vulnerable79.html");
+    f << "<html><body> User Say " << userInput << "</body></html>";
 
     cout << "Data written to vulnerable79.html" << endl;
     return 0;
